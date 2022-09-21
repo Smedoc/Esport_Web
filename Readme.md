83b99667ff3648a8ec696ae3c9198712c8123fda
Smedo Cussehi <smedocussehi@gmail.com>
2022-09-21 13:41:47 +0100

    Projecto Web Nlw

---
+++ Readme.md
@@ -0,0 +1,11 @@
+#   Projecto NlW_Esport
+
+Desafio Concluido da parte Web da nova NlW da Rocketseat.
+
+# Aproveitamento
+
+Apreendi muito com essa intensiva NlW em Uma semanda de Codigo...
+
+# Tecnologias
+
+React.js, Axios...
---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+import * as Dialog from'@radix-ui/react-dialog'
+import { CreateModal } from './components/form/createModel'
+import axios from 'axios';
+
+import './syles/main.css'
+import Logo from './image/Logo.svg';
+
+
+interface Game{
+  id: string,
+  title: string,
+  bannerUrl: string,
+  _count: {
+    ads: number
+  }
+}
+
+
+function App() {
+
+  const [ game, setGame ] = useState<Game[]>([])
+
+  useEffect(() => {
+
+    axios('http://localhost:3333/games')
+    .then(Response => {
+      setGame(Response.data)
+    })
+
+  },[])
+
+  return(
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={Logo} alt="" />
+      <h1 className='text-6xl text-white font-black mt-15'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text ' >duo</span> esta aqui!
+        </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16' >
+
+        {game.map(game => {
+          return (
+            <GameBanner
+              key={game.id} 
+              bannerUrl={game.bannerUrl} 
+              title={game.title}
+              adsCount={game._count.ads} 
+            />
+          )
+        })}
+
+
+      </div>  
+
+      <Dialog.Root>
+        <CreateAdBanner/>
+
+        <CreateModal/>
+
+      </Dialog.Root>
+
+    </div>
+
+    
+  )
+}
+
+export default App
